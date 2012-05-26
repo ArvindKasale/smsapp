57e50c20d461d35ac5cf6e878f8387c47a027c4e
Girish <girishsambhare@gmail.com>
2012-05-26 17:36:18 +0530

    reports commited

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,16 +19,16 @@ class ReportsController < ApplicationController
    rescue
       flash[:notice] = "Please use proper date format"  
       @message = "Sale Details for All Salesmans"     
-      @sale_items = SaleItem.find(:all, :order => "sale_date desc", :include => [:sales_user, {:product =>            [:category, :product_prices]}])     
+      @sale_items = SaleItem.find(:all, :order => "sale_date desc", :include => [:sales_users, {:product =>            [:category, :product_prices]}])     
       render :action => :index  
 
    else
 
-   @message = "Sale Details for All Salesmans from #{params[:from_date]} To #{params[:to_date]}"     
+   @message = "Sale Details for All Salesmans From #{params[:from_date]} To #{params[:to_date]}"     
    @sale_items = SaleItem.find(:all,
     :conditions => ["sale_date between ? and ?",start_date, end_date], 
     :order => "sale_date desc",
-    :include => [:sales_user, {:product => [:category, :product_prices]}]
+    :include => [:sales_users, {:product => [:category, :product_prices]}]
     )     
 
    end     
@@ -50,11 +50,33 @@ class ReportsController < ApplicationController
   end
 
   logger.info @sum
+
+
+  respond_to do |format|
+      format.xls { send_data(@sale_items.to_xls(:except => [:id, :updated_at]), :filename => "Overall_sales_#{Time.now.strftime("%Y%m%d-%H%M%S")}.xls") }
+      format.html
+
+
+   end
+    
+  
+  
   
   end
 
 
  def salesman
+
+   query = "1 "
+   query << "and area like '%#{params[:area]}%'" if params[:area] != ""
+   query << "and city like '%#{params[:city]}%'" if params[:city] != ""
+   query << "and district like '%#{params[:district]}%'" if params[:district] != ""
+   query << "and product_id like '%#{params[:product]}%'" if params[:product] != ""
+   query << "and products.category_id like '%#{params[:category]}%'" if params[:category] != ""   
+   query << "and user_id like '%#{params[:user]}%'" if params[:user] != ""   
+
+   logger.info "-----"     
+   logger.info query     
 
    if params[:from_date] and params[:to_date]     
 
@@ -70,17 +92,26 @@ class ReportsController < ApplicationController
    rescue
       flash[:notice] = "Please use proper date format"  
       @message = "Sale Details for All Salesmans"     
-      @sale_items = SaleItem.find(:all, :order => "sale_date desc", :include => [:sales_user, {:product =>            [:category, :product_prices]}])     
+      @sale_items = SaleItem.find(:all,
+      :from => "sale_items,products,categories, sales_users",
+      :select => "sale_items.sale_date as created_at, sales_users.name as user_name, categories.name as category, products.name as product, sales_users.area as area, sales_users.city as city, sales_users.district as district",
+      :conditions => "sale_items.product_id = products.id and products.category_id = categories.id and sale_items.user_id = sales_users.id and #{query}",
+      :order => "sale_date desc"
+      )
+      
       render :action => :index  
 
    else
 
-   @message = "Sale Details for All Salesmans from #{params[:from_date]} To #{params[:to_date]}"     
+   @message = "Sale Details for All Salesmans From #{params[:from_date]} To #{params[:to_date]}"     
+
    @sale_items = SaleItem.find(:all,
-    :conditions => ["sale_date between ? and ?",start_date, end_date], 
-    :order => "sale_date desc",
-    :include => [:sales_user, {:product => [:category, :product_prices]}]
-    )     
+      :from => "sale_items,products,categories, sales_users",
+      :select => "sale_items.sale_date as created_at, sales_users.name as user_name, categories.name as category, products.name as product, sales_users.area as area, sales_users.city as city, sales_users.district as district",
+      :conditions => ["(sale_date between ? and ? ) and sale_items.product_id = products.id and products.category_id = categories.id and sale_items.user_id = sales_users.id and #{query}",start_date, end_date], 
+      :order => "sale_date desc"
+      )
+
 
    end     
    
@@ -89,14 +120,19 @@ class ReportsController < ApplicationController
     else 
 
    @message = "Sale Details for All Salesmans"     
-   @sale_items = SaleItem.find(:all, :order => "sale_date desc", :include => [:sales_user, {:product => [:category, :product_prices]}])     
+      @sale_items = SaleItem.find(:all,
+      :from => "sale_items,products,categories, sales_users",
+      :select => "sale_items.sale_date as created_at, sales_users.name as user_name, categories.name as category, products.name as product, sales_users.area as area, sales_users.city as city, sales_users.district as district",
+      :conditions => "sale_items.product_id = products.id and products.category_id = categories.id and sale_items.user_id = sales_users.id and #{query}",
+      :order => "sale_date desc"
+      )
    
   end
   @sum = 0.0
   
   @sale_items.each do |s|
-
-  @sum += s.product.price.to_f
+  logger.info s.product
+#  @sum += Product.find_by_name(s.product).price.to_f
 
   end
 
